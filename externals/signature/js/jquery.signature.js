eb72b7bb04bbc15947696c35f30e01977cdc1965
Petr Saganov <saganoff@gmail.com>
2012-04-06 17:49:41 +0300

    Implement populate method

--- externals/signature/js/jquery.signature.js
+++ externals/signature/js/jquery.signature.js
@@ -1,6 +1,7 @@
 (function($){
 
-     var def = {canvas: {width: 200, height: 100}, lines:[]};
+     var def = {canvas: {width: 200, height: 100, lineWidth: 1, lineCap: 'round', fillStyle: '#fff', strokeStyle: '#444'},
+                lines:[]};
 
      var _populate = function(el, lines)
      {
@@ -8,8 +9,72 @@
          if(!_isSignatureEqual($this.data('signature').lines, lines)){
              //signature blured
          }
-         
+  
+         _clear(el);
+         if(lines && lines.length){
+             var lines_number = lines.length;
+             for(var line = 0; line < lines_number; line++){
+                 var points_number = lines[line].length;
+                 if(points_number){
+                     _start(el, {x: lines[line][0], y: lines[line][1]});
+                     for(var point = 2; point < points_number; point += 2){
+                         _addPoint(el, {x: lines[line][point], y: lines[line][point+1]});
+                     }
+                     _close(el);
+                 }
+             }
+         }
+
          $this.data('signature').lines = lines;
+     };
+
+     var _clear = function(el)
+     {
+         var $this = $(el);
+
+         $this.data('signature').lines = [];
+         $this.data('signature').context.clearRect(0, 0, $this.data('signature').width, $this.data('signature').height);
+         return true;
+     };
+     
+     var _start = function(el, p)
+     {
+         var $this = $(el);
+
+         _linesPush(el);
+         $this.data('signature').context.beginPath();
+         if (p){
+             $this.data('signature').context.moveTo(p.x, p.y);
+             _linesPush(el, p);
+         }
+     };
+
+     var _addPoint = function(el, p)
+     {
+         var $this = $(el);
+
+         _linesPush(el, p);
+         $this.data('signature').context.lineTo(p.x, p.y);
+         $this.data('signature').context.stroke();
+         return true;
+     };
+
+     var _linesPush = function(el, p)
+     {
+         var $this = $(el);
+         
+         if(p){
+             $this.data('signature').lines[$this.data('signature').lines.length - 1].push(p);
+            } else {
+                $this.data('signature').lines.push([]);
+            }
+     };
+
+     var _close = function(el)
+     {
+         var $this = $(el);
+         $this.data('signature').context.stroke();
+         $this.data('signature').context.closePath();
      };
 
      var _isSignatureEqual = function(first, second)
@@ -32,12 +97,22 @@
                            var $this = $(this),
                            //data = $this.data('signature');
                            options = $.extend({}, def, $this.data('signature'), options);
-                           $this.append($('<canvas />',
+                           var canvas = $('<canvas />',
                                           {width:  options.canvas.width,
-                                           height: options.canvas.height}))
+                                           height: options.canvas.height});
+                           $this.append(canvas)
                                .css('width',  options.canvas.width  +'px')
                                .css('height', options.canvas.height/2 +'px')
                                .addClass('signature');
+
+                           var context         = canvas[0].getContext('2d');
+                           context.lineWidth   = options.canvas.lineWidth;//1
+                           context.lineCap     = options.canvas.lineCap;// "round";
+                           context.fillStyle   = options.canvas.fillStyle;//"#fff";
+                           context.strokeStyle = options.canvas.strokeStyle;//"#444";
+                           options.context = context;
+
+                           $this.data('signature', options);
                            
                            _populate(this, options.lines);
                            
@@ -47,7 +122,6 @@
                                $this.removeClass('signed');
                            }
                            
-                           $this.data('signature', options);
                        });
          },
 
@@ -67,7 +141,6 @@
          },
          
          populate:   function(lines) {},
-         isSigned:   function() {},
          show: function() {},
          hide: function() {},
          update: function(content) {}
--- js/main.js
+++ js/main.js
@@ -1,4 +1,15 @@
+var example = {
+    signatureA: [[1,2,3,4,5,6,7,8,9,10], [30,29,28,27,26,25,24,23,22,21]],
+    signatureC: [[11,12,13,14,15,16,17,18,19,10], [80,79,78,77,76,75,74,73,72,71], [30,29,28,27,26,25,24,23,22,21]]
+};
+
 $(document)
     .ready(function() {
-              $(".signature").signature();
+               $("[data-signature]")
+                   .each(function(i, el){
+                             var id = $(el).data('signature').id;
+                             $(el).data('signature').lines = example[id] || [];
+                         });
+
+               $("[data-signature]").signature();
            });
