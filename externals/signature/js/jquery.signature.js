130af62fd5a152a713b9fe5022f41dcf8b8f6f85
Petr Saganov <saganoff@gmail.com>
2012-04-10 23:42:49 +0300

    Replace the all context methods into separate object 'context'

--- externals/signature/js/jquery.signature.js
+++ externals/signature/js/jquery.signature.js
@@ -1,241 +1,240 @@
 (function($){
+    
+    var def = {canvas: {width: 400, height: 200, scale: 2, lineWidth: 1, lineCap: 'round', fillStyle: '#fff', strokeStyle: '#444'},
+               lines:[]};
+    
+    var context = function(canvas, options)
+    {
+        var lines               = [],
+            context             = canvas.getContext('2d');
+            context.lineWidth   = options.lineWidth   || 1;
+            context.lineCap     = options.lineCap     || "round";
+            context.fillStyle   = options.fillStyle   || "#fff";
+            context.strokeStyle = options.strokeStyle || "#444";
+        
+        return {
+            
+            populate: function(_lines)
+            {
+                if(!this.isSignatureEqual(lines, _lines)){
+                    //signature blured
+                }
+                
+                this.clear();
+                if(_lines && _lines.length){
+                    var lines_number = _lines.length;
+                    for(var line = 0; line < lines_number; line++){
+                        var points_number = _lines[line].length;
+                        if(points_number){
+                            this.start({x: _lines[line][0], y: _lines[line][1]});
+                            for(var point = 2; point < points_number; point += 2){
+                                this.addPoint({x: _lines[line][point], y: _lines[line][point+1]});
+                            }
+                            this.close();
+                        }
+                    }
+                }
+                
+                lines = _lines;                
+            },
+            
+            clear: function()
+            {
+                lines = [];
+                context.clearRect(0, 0, $(canvas).width(), $(canvas).height());
+                return true;
+            },
+            
+            start: function(p)
+            {
+                this._linesPush();
+                context.beginPath();
+                if (p){
+                    context.moveTo(p.x, p.y);
+                    this._linesPush(p);
+                }
+            },
+            
+            addPoint: function(p)
+            {
+                this._linesPush(p);
+                context.lineTo(p.x, p.y);
+                context.stroke();
+                return true;
+            },
 
-     var def = {canvas: {width: 400, height: 200, scale: 2, lineWidth: 1, lineCap: 'round', fillStyle: '#fff', strokeStyle: '#444'},
-                lines:[]};
+            _linesPush: function(p)
+            {
+                if(p){
+                    lines[lines.length - 1].push(p);
+                } else {
+                    lines.push([]);
+                }
+            },
 
-     var _populate = function(el, lines)
-     {
-         var $this = $(el);
-         if(!_isSignatureEqual($this.data('signature').lines, lines)){
-             //signature blured
-         }
-  
-         _clear(el);
-         if(lines && lines.length){
-             var lines_number = lines.length;
-             for(var line = 0; line < lines_number; line++){
-                 var points_number = lines[line].length;
-                 if(points_number){
-                     _start(el, {x: lines[line][0], y: lines[line][1]});
-                     for(var point = 2; point < points_number; point += 2){
-                         _addPoint(el, {x: lines[line][point], y: lines[line][point+1]});
-                     }
-                     _close(el);
-                 }
-             }
-         }
+            close: function()
+            {
+                context.stroke();
+                context.closePath();
+            },
 
-         $this.data('signature').lines = lines;
-     };
+            isSignatureEqual: function(first, second)
+            {
+                if (first && first.length != second.length){return false;}
+                return (JSON.stringify(first) == JSON.stringify(second));
+            },
+            
+            isSigned: function()
+            {
+                return (lines && !!lines.length);
+            },
 
-     var _clear = function(el)
-     {
-         var $this = $(el);
-
-         $this.data('signature').lines = [];
-         $this.data('signature').context.clearRect(0, 0, $this.data('signature').width, $this.data('signature').height);
-         return true;
-     };
-     
-     var _start = function(el, p)
-     {
-         var $this = $(el);
-
-         _linesPush(el);
-         $this.data('signature').context.beginPath();
-         if (p){
-             $this.data('signature').context.moveTo(p.x, p.y);
-             _linesPush(el, p);
-         }
-     };
-
-     var _addPoint = function(el, p)
-     {
-         var $this = $(el);
-
-         _linesPush(el, p);
-         $this.data('signature').context.lineTo(p.x, p.y);
-         $this.data('signature').context.stroke();
-         return true;
-     };
-
-     var _linesPush = function(el, p)
-     {
-         var $this = $(el);
-         
-         if(p){
-             $this.data('signature').lines[$this.data('signature').lines.length - 1].push(p);
-            } else {
-                $this.data('signature').lines.push([]);
+            draw: function(img)
+            {
+                context.drawImage(img, 0, 0);
             }
-     };
+        };
+    };
+   
+    var showPopup = function(e)
+    {
+        var container = $('<div />', {'class': 'popup_signature'})
+            .css('width',  def.canvas.width+'px')
+            .css('height', def.canvas.height+'px');
+        var canvas = $('<canvas />')
+            .attr('width',  def.canvas.width)
+            .attr('height', def.canvas.height);
+        
+        //grab the context from your destination canvas
+        var ctx = context(canvas[0], def.canvas);
+        //call its drawImage() function passing it the source canvas directly
+        ctx.draw(e.target);
 
-     var _close = function(el)
-     {
-         var $this = $(el);
-         $this.data('signature').context.stroke();
-         $this.data('signature').context.closePath();
-     };
+        var hidePopup = function(e)
+        {
+            $('div.popup_signature').remove();
+        };
+        
+        // TODO: There has to be the way to reload this methods
+        var callback = {
+            ok: function(e){
+                console.log('The signature is signed');
+                // TODO: Store the new lines into appropriatted signature
+                //       Must decide if there should be:
+                //         - adding additional lines to existed ones
+                //         - replacing existed lines by new ones
+                //         - removing all old lines (clean signature)
+                //       And refresh it
+                hidePopup();
+            },
+            cancel: function(e){
+                console.log('The signature was closed');
+                hidePopup();
+            },
+            clear: function(e){
+                console.log('The signature was cleared');
+                ctx.clear();
+            }
+        };
+        
+        var button = {
+            ok:     $('<input type="button" value="OK" />').click(callback.ok),
+            cancel: $('<input type="button" value="Cancel" />').click(callback.cancel),
+            clear:  $('<input type="button" value="Clear " />').click(callback.clear)
+        };
+        
+        $(container).append(canvas).append(button.ok).append(button.cancel).append(button.clear);
+        $('body').append(container);
+    };
+    
+    
+    var methods = {
+        init: function(options)
+        {
+            options = $.extend({}, def, options);
+            return this
+                .each(function(){
+                    var $this = $(this),
+                    //data = $this.data('signature');
+                    options = $.extend({}, def, $this.data('signature'), options);
+                    var size = {width: options.canvas.width  / options.canvas.scale,
+                                height: options.canvas.height / options.canvas.scale};
+                    var canvas = $('<canvas />', {style:  'width:'+size.width+'px;height:'+size.height+'px;'})
+                        .attr('width',  options.canvas.width)
+                        .attr('height', options.canvas.height);
+                    $this.append(canvas)
+                        .css('width',  size.width+'px')
+                        .css('height', size.height/2 +'px')
+                        .addClass('signature')
+                        .click(showPopup);
+                    
+                    options.context = context(canvas[0], options.canvas);
+                    options.context.populate(options.lines);
+                    
+                    if(options.context.isSigned()){
+                        $this.addClass('signed');
+                    } else {
+                        $this.removeClass('signed');
+                    }
+                    
+                    $this.data('signature', options);
+                    
+                });
+        },
 
-     var _isSignatureEqual = function(first, second)
-     {
-         if (first && first.length != second.length){return false;}
-         return (JSON.stringify(first) == JSON.stringify(second));
-     };
-
-     var _isSigned = function(el)
-     {
-         return ($(el).data('signature').lines && !!$(el).data('signature').lines.length);
-     };
-
-
-
-     var showPopup = function(e)
-     {
-         var container = $('<div />', {'class': 'popup_signature'})
-             .css('width',  def.canvas.width+'px')
-             .css('height', def.canvas.height+'px');
-         var canvas = $('<canvas />')
-             .attr('width',  def.canvas.width)
-             .attr('height', def.canvas.height);
-
-         //grab the context from your destination canvas
-         var ctx = canvas[0].getContext('2d');
-         //call its drawImage() function passing it the source canvas directly
-         ctx.drawImage(e.target, 0, 0);
-         
-         // TODO: There has to be the way to reload this methods
-         var callback = {
-             ok: function(e){
-                 console.log('The signature is signed');
-                 // TODO: Store the new lines into appropriatted signature
-                 //       Must decide if there should be:
-                 //         - adding additional lines to existed ones
-                 //         - replacing existed lines by new ones
-                 //         - removing all old lines (clean signature)
-                 //       And refresh it
-                 hidePopup();
-             },
-             cancel: function(e){
-                 console.log('The signature was closed');
-                 hidePopup();
-             },
-             clear: function(e){
-                 console.log('The signature was cleared');
-                 ctx.clearRect(0, 0, canvas[0].width, canvas[0].height);
-             }
-         };
-             
-         var button = {
-             ok:     $('<input type="button" value="OK" />').click(callback.ok),
-             cancel: $('<input type="button" value="Cancel" />').click(callback.cancel),
-             clear:  $('<input type="button" value="Clear " />').click(callback.clear)
-         };
-
-         $(container).append(canvas).append(button.ok).append(button.cancel).append(button.clear);
-         $('body').append(container);
-
-
-     };
-     
-     var hidePopup = function(e)
-     {
-         $('div.popup_signature').remove();
-     };
-
-     var methods = {
-         init: function(options)
-         {
-             options = $.extend({}, def, options);
-             return this
-                 .each(function(){
-                           var $this = $(this),
-                           //data = $this.data('signature');
-                           options = $.extend({}, def, $this.data('signature'), options);
-                           var size = {width: options.canvas.width  / options.canvas.scale,
-                                      height: options.canvas.height / options.canvas.scale};
-                           var canvas = $('<canvas />', {style:  'width:'+size.width+'px;height:'+size.height+'px;'})
-                               .attr('width',  options.canvas.width)
-                               .attr('height', options.canvas.height);
-                           $this.append(canvas)
-                               .css('width',  size.width+'px')
-                               .css('height', size.height/2 +'px')
-                               .addClass('signature')
-                               .click(showPopup);
-
-                           var context         = canvas[0].getContext('2d');
-                           context.lineWidth   = options.canvas.lineWidth;//1
-                           context.lineCap     = options.canvas.lineCap;// "round";
-                           context.fillStyle   = options.canvas.fillStyle;//"#fff";
-                           context.strokeStyle = options.canvas.strokeStyle;//"#444";
-                           options.context = context;
-
-                           $this.data('signature', options);
-                           
-                           _populate(this, options.lines);
-                           
-                           if(_isSigned(this)){
-                               $this.addClass('signed');
-                           } else {
-                               $this.removeClass('signed');
-                           }
-                           
-                       });
-         },
-
-         destroy: function()
-         {
-             return this.each(function(){
-                                  var $this = $(this),
-                                  data = $this.data('signature');
-
-                                  // Namespacing FTW
-                                  $(window).unbind('.signature');
-                                  data.signature.remove();
-                                  $this.removeData('signature');
-
-                              });
-
-         },
-         
-         populate:   function(lines) {},
-         show: function() {},
-         hide: function() {},
-         update: function(content) {}
-     };
-
-     $.fn.signature = function(method)
-     {
-         if (methods[method]){
-             return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
-         } else if (typeof method === 'object' || ! method) {
-             return methods.init.apply(this, arguments);
-         } else {
-             $.error('Method ' +  method + ' does not exist on jQuery.signature');
-         }
-     };
-     
-     /*
-      * Class and Function
-      */
-/*
-     var MyClass = function(el){
-         var $el = $(el);
-         
-         // Put all class stuff here
-
-         
-         // Attach the instance of this object
-         // to the jQuery wrapped DOM node
-         $el.data('MyClass', this);
-     };
-     
-     $.fn.signature = function(method)
-     {
-         return this.each(function(){
-                              (new ClassSignature(this));
-                          });
-     };
-*/
-
- })( jQuery );
+        destroy: function()
+        {
+            return this.each(function(){
+                var $this = $(this),
+                data = $this.data('signature');
+                
+                // Namespacing FTW
+                $(window).unbind('.signature');
+                data.signature.remove();
+                $this.removeData('signature');
+                
+            });
+            
+        },
+        
+        populate:   function(lines) {},
+        show: function() {},
+        hide: function() {},
+        update: function(content) {}
+    };
+    
+    $.fn.signature = function(method)
+    {
+        if (methods[method]){
+            return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+        } else if (typeof method === 'object' || ! method) {
+            return methods.init.apply(this, arguments);
+        } else {
+            $.error('Method ' +  method + ' does not exist on jQuery.signature');
+        }
+    };
+    
+    /*
+     * Class and Function
+     */
+    /*
+      var MyClass = function(el){
+      var $el = $(el);
+      
+      // Put all class stuff here
+      
+      
+      // Attach the instance of this object
+      // to the jQuery wrapped DOM node
+      $el.data('MyClass', this);
+      };
+      
+      $.fn.signature = function(method)
+      {
+      return this.each(function(){
+      (new ClassSignature(this));
+      });
+      };
+    */
+    
+})( jQuery );
