e8128d19bf5414f238b8fb15561c2db42823d87a
Petr Saganov <saganoff@gmail.com>
2012-04-12 01:43:02 +0300

    Fix the bug with wrong canvas

--- externals/signature/js/jquery.signature.js
+++ externals/signature/js/jquery.signature.js
@@ -3,7 +3,7 @@
     var def = {canvas: {width: 400, height: 200, scale: 2, lineWidth: 1, lineCap: 'round', fillStyle: '#fff', strokeStyle: '#444'},
                lines:[]};
     
-    var context = function(canvas, options)
+    var signature_context = function(canvas, options)
     {
         var lines               = [],
             blured              = false,
@@ -111,113 +111,6 @@
    
     var popup = {
         
-        canvas: undefined,
-        signature: undefined,
-        
-        getXY: function(e)
-        {
-            var x, y;
-            var offsety = popup.canvas.offsetTop || 0;
-            var offsetx = popup.canvas.offsetLeft || 0;
-
-            if (e.changedTouches && e.changedTouches[0])
-            {
-                x = e.changedTouches[0].pageX;
-                y = e.changedTouches[0].pageY;
-            }
-            else if (e.layerX || 0 == e.layerX)
-            {
-                x = e.layerX;
-                y = e.layerY;
-            }
-            else if (e.offsetX || 0 == e.offsetX)
-            {
-                x = e.offsetX;
-                y = e.offsetY;
-            }
-            console.log(popup.canvas.offsetTop);
-            return {
-                x: x - offsetx,
-                y: y - offsety
-            };
-        },
-
-        events: {
-            startCapture: function(e)
-            {
-                e.preventDefault();
-                e.stopPropagation();
-                
-                document.body.theSig = this;
-                
-                //IJS.addEvent('move', signature.getCanvas(), moveEvent);
-                //IJS.addEvent('out',   signature.getCanvas(), handleOut);
-                //IJS.addEvent('stop',  signature.getCanvas(), stopCapture);
-                popup.canvas.addEventListener('mousemove', popup.events.moveEvent, false);
-                popup.canvas.addEventListener('touchmove', popup.events.moveEvent, false);
-                popup.canvas.addEventListener('mouseup',   popup.events.stopCapture,   false);
-                popup.canvas.addEventListener('touchend',  popup.events.stopCapture,   false);
-
-                document.body.addEventListener('mouseup',  popup.events.stopCapture, false);
-                document.body.addEventListener('touchend', popup.events.stopCapture, false);
-
-                var p = popup.getXY(e);
-                popup.signature.start(p);
-                
-                return true;
-            },
-        
-            stopCapture: function(e)
-            {
-                popup.signature.close();
-                //IJS.removeEvent('move', signature.getCanvas(), moveEvent);
-                //IJS.removeEvent('out',  signature.getCanvas(), handleOut);
-                //IJS.removeEvent('stop', signature.getCanvas(), stopCapture);
-                popup.canvas.removeEventListener('mousemove', popup.events.moveEvent, false);
-                popup.canvas.removeEventListener('touchmove', popup.events.moveEvent, false);
-                popup.canvas.removeEventListener('mouseup',   popup.events.stopCapture,   false);
-                popup.canvas.removeEventListener('touchend',  popup.events.stopCapture,   false);
-            
-                document.body.removeEventListener('mouseup',  popup.events.stopCapture, false);
-                document.body.removeEventListener('touchend', popup.events.stopCapture, false);
-            },
-        
-            moveEvent: function(e)
-            {
-                e.preventDefault();
-                e.stopPropagation();
-                e.cancelBubble = true;
-            
-                var p = popup.getXY(e);
-                
-                if (typeof(document.body.theSig) == 'boolean')
-                {
-                    popup.events.stopCapture(e);
-                    return true;
-                }
-                
-                popup.signature.addPoint(p);
-                return true;
-            },
-        
-            handleOut: function(e)
-            {
-                e.preventDefault();
-                
-                //save current line and empty stack
-                if (document.body.theSig)
-                {
-                    var p = popup.getXY(e);
-                    //signature.addPoint(p);
-                    popup.signature.start();
-                };
-                
-                //IJS.addEvent('stop', document.body, function(e){this.theSig = false;});
-                document.body.addEventListener('mouseup',  function(e){this.theSig = false;});
-                document.body.addEventListener('touchend', function(e){this.theSig = false;});
-            }
-        },
-
         show: function(e)
         {
             var container = $('<div />', {'class': 'popup_signature'})
@@ -228,18 +121,12 @@
                 .attr('height', def.canvas.height);
 
            
-            var signature = e.target.parentNode;
+            var signature = $(e.target.parentNode).data('signature').context;
             //grab the context from your destination canvas
-            var ctx = context(canvas[0], def.canvas);
+            var ctx = signature_context(canvas[0], def.canvas);
             //call its drawImage() function passing it the source canvas directly
             ctx.draw(e.target);
-
-            popup.canvas    = canvas[0];
-            popup.signature = $(signature).data('signature').context;
-
-            //IJS.addEvent('start', signature.getCanvas(), startCapture);
-            popup.canvas.addEventListener('mousedown',  popup.events.startCapture, false);
-            popup.canvas.addEventListener('touchstart', popup.events.startCapture, false);
+            //ctx.populate(signature.getLines());
 
             // TODO: There has to be the way to reload this methods
             var callback = {
@@ -252,14 +139,14 @@
                     //         - removing all old lines (clean signature)
                     //       And refresh it
                     if(ctx.isBlured()){
-                        $(signature).data('signature').context.populate(ctx.getLines());
+                        signature.populate(ctx.getLines());
                     }
 
                     // TODO: Think, how to replace it into event handler
-                    if($(signature).data('signature').context.isSigned()){
-                        $(signature).addClass('signed');
+                    if(signature.isSigned()){
+                        $(e.target.parentNode).addClass('signed');
                     } else {
-                        $(signature).removeClass('signed');
+                        $(e.target.parentNode).removeClass('signed');
                     }                        
 
                     popup.hide(e);
@@ -279,9 +166,118 @@
                 cancel: $('<input type="button" value="Cancel" />').click(callback.cancel),
                 clear:  $('<input type="button" value="Clear " />').click(callback.clear)
             };
-            
+
             $(container).append(canvas).append(button.ok).append(button.cancel).append(button.clear);
             $('body').append(container);
+
+            canvas = canvas[0];
+            var listener = {
+                getXY: function(e)
+                {
+                    var x, y;
+                    var offsety = canvas.offsetTop || 0;
+                    var offsetx = canvas.offsetLeft || 0;
+                    
+                    if (e.changedTouches && e.changedTouches[0])
+                    {
+                        x = e.changedTouches[0].pageX;
+                        y = e.changedTouches[0].pageY;
+                    }
+                    else if (e.layerX || 0 == e.layerX)
+                    {
+                        x = e.layerX;
+                        y = e.layerY;
+                    }
+                    else if (e.offsetX || 0 == e.offsetX)
+                    {
+                        x = e.offsetX;
+                        y = e.offsetY;
+                    }
+
+                    return {
+                        x: x - offsetx,
+                        y: y - offsety
+                    };
+                },
+            
+                startCapture: function(e)
+                {
+                    e.preventDefault();
+                    e.stopPropagation();
+                    
+                    document.body.theSig = this;
+                
+                    //IJS.addEvent('move', signature.getCanvas(), moveEvent);
+                    //IJS.addEvent('out',   signature.getCanvas(), handleOut);
+                    //IJS.addEvent('stop',  signature.getCanvas(), stopCapture);
+                    canvas.addEventListener('mousemove', listener.moveEvent, false);
+                    canvas.addEventListener('touchmove', listener.moveEvent, false);
+                    canvas.addEventListener('mouseup',   listener.stopCapture,   false);
+                    canvas.addEventListener('touchend',  listener.stopCapture,   false);
+
+                    document.body.addEventListener('mouseup',  listener.stopCapture, false);
+                    document.body.addEventListener('touchend', listener.stopCapture, false);
+
+                    var p = listener.getXY(e);
+                    ctx.start(p);
+                    
+                    return true;
+                },
+                
+                stopCapture: function(e)
+                {
+                    ctx.close();
+                    //IJS.removeEvent('move', signature.getCanvas(), moveEvent);
+                    //IJS.removeEvent('out',  signature.getCanvas(), handleOut);
+                    //IJS.removeEvent('stop', signature.getCanvas(), stopCapture);
+                    canvas.removeEventListener('mousemove', listener.moveEvent, false);
+                    canvas.removeEventListener('touchmove', listener.moveEvent, false);
+                    canvas.removeEventListener('mouseup',   listener.stopCapture,   false);
+                    canvas.removeEventListener('touchend',  listener.stopCapture,   false);
+                    
+                    document.body.removeEventListener('mouseup',  listener.stopCapture, false);
+                    document.body.removeEventListener('touchend', listener.stopCapture, false);
+                },
+                
+                moveEvent: function(e)
+                {
+                    e.preventDefault();
+                    e.stopPropagation();
+                    e.cancelBubble = true;
+                    
+                    var p = listener.getXY(e);
+                
+                    if (typeof(document.body.theSig) == 'boolean')
+                    {
+                        listener.stopCapture(e);
+                        return true;
+                    }
+                
+                    ctx.addPoint(p);
+                    return true;
+                },
+        
+                handleOut: function(e)
+                {
+                    e.preventDefault();
+                    
+                    //save current line and empty stack
+                    if (document.body.theSig)
+                    {
+                        var p = listener.getXY(e);
+                        //signature.addPoint(p);
+                        ctx.start();
+                    };
+                
+                    //IJS.addEvent('stop', document.body, function(e){this.theSig = false;});
+                    document.body.addEventListener('mouseup',  function(e){this.theSig = false;});
+                    document.body.addEventListener('touchend', function(e){this.theSig = false;});
+                }
+            };
+
+            //IJS.addEvent('start', signature.getCanvas(), startCapture);
+            canvas.addEventListener('mousedown',  listener.startCapture, false);
+            canvas.addEventListener('touchstart', listener.startCapture, false);
         },
 
         hide: function(e)
@@ -311,7 +307,7 @@
                         .addClass('signature')
                         .click(popup.show);
                     
-                    options.context = context(canvas[0], options.canvas);
+                    options.context = signature_context(canvas[0], options.canvas);
                     options.context.populate(options.lines);
                     
                     if(options.context.isSigned()){
