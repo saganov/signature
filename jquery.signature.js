e6eaafcd928bbdd9e012071037db8b13267ae0b1
Petr Saganov <saganoff@gmail.com>
2012-07-04 01:00:47 +0300

    Replace the canvas context related code into separate class

--- jquery.signature.js
+++ jquery.signature.js
@@ -3,142 +3,6 @@
     var def = {canvas: {width: 400, height: 200, scale: 2, lineWidth: 1, lineCap: 'round', fillStyle: '#fff', strokeStyle: '#444'},
                lines:[]};
     
-    var signature_context = function(canvas, options, callback)
-    {
-        var lines               = [],
-            blurred             = false,
-            cleared             = false,
-            context             = canvas.getContext('2d');
-            context.lineWidth   = options.lineWidth   || 1;
-            context.lineCap     = options.lineCap     || "round";
-            context.fillStyle   = options.fillStyle   || "#fff";
-            context.strokeStyle = options.strokeStyle || "#444";
-        
-        return {
-            
-            populate: function(_lines)
-            {
-                if(!this._isSignatureEqual(lines, _lines)){
-                    //signature blurred
-                }
-                
-                this.clear();
-                this.update(_lines);
-            },
-
-            update: function(_lines)
-            {
-                if(_lines && _lines.length){
-                    var lines_number = _lines.length;
-                    for(var line = 0; line < lines_number; line++){
-                        var points_number = _lines[line].length;
-                        if(points_number){
-                            if(typeof _lines[line][0] == 'object'){
-                                this.start(_lines[line][0]);
-                                for(var point = 1; point < points_number; point++){
-                                    this.addPoint(_lines[line][point]);
-                                }
-                            } else {
-                                this.start({x: _lines[line][0], y: _lines[line][1]});
-                                for(var point = 2; point < points_number; point += 2){
-                                    this.addPoint({x: _lines[line][point], y: _lines[line][point+1]});
-                                }                     
-                            }
-
-                            this.close();
-                        }
-                    }
-                }
-                lines = _lines;
-                cleared = false;
-                blurred  = false;
-                if(typeof callback == 'function') callback(this);
-            },
-            
-            clear: function()
-            {
-                lines = [];
-                context.clearRect(0, 0, canvas.width, canvas.height);
-                blurred = true;
-                cleared = true;
-                return true;
-            },
-            
-            start: function(p)
-            {
-                this._linesPush();
-                context.beginPath();
-                if (p){
-                    context.moveTo(p.x, p.y);
-                    this._linesPush(p);
-                }
-            },
-            
-            addPoint: function(p)
-            {
-                this._linesPush(p);
-                context.lineTo(p.x, p.y);
-                context.stroke();
-                blurred = true;
-                return true;
-            },
-
-            close: function()
-            {
-                context.stroke();
-                context.closePath();
-            },
-
-            getLines: function()
-            {
-                return lines;
-            },
-
-            isSigned: function()
-            {
-                return (lines && !!lines.length);
-            },
-
-            isBlurred: function()
-            {
-                return blurred;
-            },
-
-            isCleared: function()
-            {
-                return cleared;
-            },
-
-            getStatus: function()
-            {
-                if(blurred && cleared) return 'new';
-                else if(!blurred && cleared) return 'clean';
-                else if(blurred && !cleared) return 'updated';
-                else return '';
-            },
-
-            draw: function(img)
-            {
-                context.drawImage(img, 0, 0);
-            },
-
-            _isSignatureEqual: function(first, second)
-            {
-                if (first && first.length != second.length){return false;}
-                return (JSON.stringify(first) == JSON.stringify(second));
-            },
-
-            _linesPush: function(p)
-            {
-                if(p){
-                    lines[lines.length - 1].push(p);
-                } else {
-                    lines.push([]);
-                }
-            }
-        };
-    };
-   
     var popup = {
         
         show: function(e)
@@ -150,9 +14,9 @@
                 .attr('width',  def.canvas.width)
                 .attr('height', def.canvas.height);
 
-            var signature = $(e.data.signature).data('signature').context;
+            var signature = e.data.signature.data('signature').context;
             //grab the context from your destination canvas
-            var ctx = signature_context(canvas[0], def.canvas);
+            var ctx = $signature.context(canvas[0], def.canvas);
             //call its drawImage() function passing it the source canvas directly
             ctx.draw(e.target);
             //ctx.populate(signature.getLines());
@@ -342,7 +206,7 @@
                         .addClass('signature')
                         .click({signature: $this}, popup.show);
                     
-                    options.context = signature_context(canvas[0],
+                    options.context = $signature.context(canvas[0],
                                                         options.canvas,
                                                         function(context){
                                                             if(context.isSigned()){
---
+++ signature.js
@@ -0,0 +1,136 @@
+var $signature = (function () {
+
+    var context = function(canvas, options, callback){
+        
+        var lines         = [],
+        blurred           = false,
+        cleared           = false,
+        ctx               = canvas.getContext('2d'),
+        _isSignatureEqual = function(first, second){
+            if (first && first.length != second.length){return false;}
+            return (JSON.stringify(first) == JSON.stringify(second));},
+        _linesPush        = function(p){
+            if(p){
+                lines[lines.length - 1].push(p);
+            } else {
+                lines.push([]);
+            }};
+        
+        ctx.lineWidth   = options.lineWidth   || 1;
+        ctx.lineCap     = options.lineCap     || "round";
+        ctx.fillStyle   = options.fillStyle   || "#fff";
+        ctx.strokeStyle = options.strokeStyle || "#444";
+
+        return {
+            populate: function(_lines)
+            {
+                if(!_isSignatureEqual(lines, _lines)){
+                    //signature blurred
+                }
+                
+                this.clear();
+                this.update(_lines);
+            },
+
+            update: function(_lines)
+            {
+                if(_lines && _lines.length){
+                    var lines_number = _lines.length;
+                    for(var line = 0; line < lines_number; line++){
+                        var points_number = _lines[line].length;
+                        if(points_number){
+                            if(typeof _lines[line][0] == 'object'){
+                                this.start(_lines[line][0]);
+                                for(var point = 1; point < points_number; point++){
+                                    this.addPoint(_lines[line][point]);
+                                }
+                            } else {
+                                this.start({x: _lines[line][0], y: _lines[line][1]});
+                                for(var point = 2; point < points_number; point += 2){
+                                    this.addPoint({x: _lines[line][point], y: _lines[line][point+1]});
+                                }                     
+                            }
+                            
+                            this.close();
+                        }
+                    }
+                }
+                lines = _lines;
+                cleared = false;
+                blurred  = false;
+                if(typeof callback == 'function') callback(this);
+            },
+            
+            clear: function()
+            {
+                lines = [];
+                ctx.clearRect(0, 0, canvas.width, canvas.height);
+                blurred = true;
+                cleared = true;
+                return true;
+            },
+            
+            start: function(p)
+            {
+                _linesPush();
+                ctx.beginPath();
+                if (p){
+                    ctx.moveTo(p.x, p.y);
+                    _linesPush(p);
+                }
+            },
+            
+            addPoint: function(p)
+            {
+                _linesPush(p);
+                ctx.lineTo(p.x, p.y);
+                ctx.stroke();
+                blurred = true;
+                return true;
+            },
+
+            close: function()
+            {
+                ctx.stroke();
+                ctx.closePath();
+            },
+
+            getLines: function()
+            {
+                return lines;
+            },
+
+            isSigned: function()
+            {
+                return (lines && !!lines.length);
+            },
+
+            isBlurred: function()
+            {
+                return blurred;
+            },
+
+            isCleared: function()
+            {
+                return cleared;
+            },
+
+            getStatus: function()
+            {
+                if(blurred && cleared) return 'new';
+                else if(!blurred && cleared) return 'clean';
+                else if(blurred && !cleared) return 'updated';
+                else return '';
+            },
+
+            draw: function(img)
+            {
+                ctx.drawImage(img, 0, 0);
+            }
+        };
+    };
+    
+    return{
+        context: context
+    };
+})();
